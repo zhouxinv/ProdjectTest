058c557c7290dd522840e4f0be8b5300c0964b02
gewei0701 <gewei0701@autohome.com.cn>
2021-01-15 15:39:11 +0800

    * 添加初始化文件

---
+++ testxcporj/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  testxcporj
+//
+//  Created by gewei on 2021/1/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ testxcporj/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testxcporj
+//
+//  Created by gewei on 2021/1/15.
+//
+
+#import "ViewController.h"
+#import <shareKit/shareKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    VVshareSDK *test = [[VVshareSDK alloc] init];
+    [test sayHello];
+}
+
+
+@end
